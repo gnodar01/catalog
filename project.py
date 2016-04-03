e6eba57e491f9867527aca2b65c2aa04aec7b297
Nodar Gogoberidze <gnodar01@gmail.com>
2016-04-02 23:19:49 -0400

    Populate URL schema

--- project.py
+++ project.py
@@ -14,6 +14,89 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/')
+@app.route('/catalog/')
+def viewCatalogs():
+	pass
+
+@app.route('/catalog/new/')
+def newCatalog():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/edit/')
+def editCatalog():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/delete/')
+def deleteCatalog():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/')
+def viewCategories():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/new')
+def newCategory():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/edit/')
+def editCategory():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/delete/')
+def deleteCategory():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/')
+def viewRecords():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/newTemplate/')
+def newRecordTemplate():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/customTemplate/')
+def newCustomTemplate():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/new/')
+def addRecord():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/<int:record_id>/edit/')
+def editRecord():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/<int:record_id>/delete/')
+def deleteRecord():
+	pass
+
+@app.route('/catalog/<int:catalog_id>/category/<int:category_id>/record/<int:record_id>/view/')
+def showRecord():
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
