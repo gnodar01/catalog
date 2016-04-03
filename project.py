06b652030219e6315651d48d9a3d70103f00919e
Nodar Gogoberidze <gnodar01@gmail.com>
2016-04-02 22:52:47 -0400

    Remove comment

--- project.py
+++ project.py
@@ -8,7 +8,6 @@ from database_setup import (Base, User, Catalog, Category, Record, Field,
 
 APPLICATION_NAME = "Catalogizer"
 
-#Connect to Database and create database session
 engine = create_engine('sqlite:///catalogizer.db')
 Base.metadata.bind = engine
 
@@ -23,4 +22,3 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
-    
