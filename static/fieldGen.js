3c178634d36bffaf7cee9782cc3f08c97cd7ee41
Nodar Gogoberidze <gnodar01@gmail.com>
2016-04-16 15:36:46 -0400

    Fix option input genration bug. Bug caused additional inputs to be unecessarily added when switching from one option field kind to another option field kind

--- static/fieldGen.js
+++ static/fieldGen.js
@@ -1,59 +1,59 @@
-function genOption(num){
-	var fkSelectId = num + "-field-kind-select";
-	var fkSelect = document.getElementById(fkSelectId);
-	var currentVal = fkSelect.value;
-	
-	var currentOptionFieldDivs = document.getElementsByClassName(num + "-option-field-div");
-
-	if (currentVal === "drop_down" || currentVal === "check_box" || currentVal === "radio") {
-		if(currentOptionFieldDivs.length === 0) {
-			var fieldDescriptionDiv = document.getElementById(num + "-field-description");
-
-			var newOptionButton = document.getElementById(num + "-new-option-button");
-			if (newOptionButton == null) {
-				newOptionButton = document.createElement("button");
-				newOptionButton.type = "button";
-				newOptionButton.id = num + "-new-option-button";
-				newOptionButton.innerHTML = "Add option";
-				newOptionButton.onclick = function() { genOption(num) };
-
-				fieldDescriptionDiv.appendChild(newOptionButton);
-			}
-
-			var optionFieldDiv = document.createElement("div");
-			optionFieldDiv.className = num + "-option-field-div";
-
-			var optionInput = document.createElement("input");
-			optionInput.name = num + "-option";
-			optionInput.class = num + "-option-input";
-			var optionInputText = document.createTextNode("Option Label:");
-
-			optionFieldDiv.appendChild(optionInputText);
-			optionFieldDiv.appendChild(document.createElement("br"));
-			optionFieldDiv.appendChild(optionInput);
-			optionFieldDiv.appendChild(document.createElement("br"));
-
-			fieldDescriptionDiv.insertBefore(optionFieldDiv, newOptionButton);
-		}
-	} else {
-		var newOptionButton = document.getElementById(num + "-new-option-button");
-		if (newOptionButton != null) {
-			newOptionButton.parentNode.removeChild(newOptionButton);
-		}
-		var optionFieldDivs = document.getElementsByClassName(num + "-option-field-div");
-		while (optionFieldDivs.length > 0) {
-			optionFieldDivs[0].parentNode.removeChild(optionFieldDivs[0]);
-		}
+function delOptions(fdNum) {
+	var addOptionBtn = document.getElementById(fdNum + "-add-option-btn");
+	if (addOptionBtn != null) {
+		addOptionBtn.parentNode.removeChild(addOptionBtn);
+	}
+	var optionFieldDivs = document.getElementsByClassName(fdNum + "-option-field-div");
+	while (optionFieldDivs.length > 0) {
+		optionFieldDivs[0].parentNode.removeChild(optionFieldDivs[0]);
 	}
 }
 
-function genField(num) {
-	var prevFieldDescriptionNum = num - 1;
+function genOption(fdNum){
+	var fkSelectId = fdNum + "-field-kind-select";
+	var fkSelect = document.getElementById(fkSelectId);
+	var currentVal = fkSelect.value;
+
+	if (currentVal === "drop_down" || currentVal === "check_box" || currentVal === "radio")  {
+		var fieldDescriptionDiv = document.getElementById(fdNum + "-field-description");
+
+		var addOptionBtn = document.getElementById(fdNum + "-add-option-btn");
+		if (addOptionBtn == null) {
+			addOptionBtn = document.createElement("button");
+			addOptionBtn.type = "button";
+			addOptionBtn.id = fdNum + "-add-option-btn";
+			addOptionBtn.innerHTML = "Add option";
+			addOptionBtn.onclick = function() { genOption(fdNum) };
+
+			fieldDescriptionDiv.appendChild(addOptionBtn);
+		}
+
+		var optionFieldDiv = document.createElement("div");
+		optionFieldDiv.className = fdNum + "-option-field-div";
+
+		var optionInput = document.createElement("input");
+		optionInput.name = fdNum + "-option";
+		optionInput.class = fdNum + "-option-input";
+		var optionInputText = document.createTextNode("Option Label:");
+
+		optionFieldDiv.appendChild(optionInputText);
+		optionFieldDiv.appendChild(document.createElement("br"));
+		optionFieldDiv.appendChild(optionInput);
+		optionFieldDiv.appendChild(document.createElement("br"));
+
+		fieldDescriptionDiv.insertBefore(optionFieldDiv, addOptionBtn);
+	} else {
+		delOptions(fdNum);
+	}
+}
+
+function genField(fdNum) {
+	var prevFieldDescriptionNum = fdNum - 1;
 	var prevFieldDescriptionId = prevFieldDescriptionNum.toString() + "-field-description";
 	var prevFieldDescription = document.getElementById(prevFieldDescriptionId);
 
 	var fdDiv = document.createElement("div");
-	fdDivNum = num.toString();
+	fdDivNum = fdNum.toString();
 	fdDiv.id = fdDivNum + "-field-description";
 
 	var fieldLabelText = document.createTextNode("Field Label:");
@@ -62,7 +62,7 @@ function genField(num) {
 
 	var fdLabel = document.createElement("input");
 	fdLabel.type = "text";
-	fdLabel.name = num + "-field-label";
+	fdLabel.name = fdNum + "-field-label";
 	fdDiv.appendChild(fdLabel);
 	fdDiv.appendChild(document.createElement("br"));
 
@@ -71,9 +71,12 @@ function genField(num) {
 	fdDiv.appendChild(document.createElement("br"));
 
 	var fdSelect = document.createElement("select");
-	fdSelect.name = num + "-field-kind";
-	fdSelect.id = num + "-field-kind-select";
-	fdSelect.onchange = function() { genOption(num) };
+	fdSelect.name = fdNum + "-field-kind";
+	fdSelect.id = fdNum + "-field-kind-select";
+	fdSelect.onchange = function() {
+		delOptions(fdNum);
+		genOption(fdNum);
+	};
 
 	var fdShortTextOption = document.createElement("option");
 	fdShortTextOption.value = "short_text";
@@ -104,8 +107,8 @@ function genField(num) {
 	fdDiv.appendChild(document.createElement("br"));
 
 	var addFieldButton = document.getElementById("add-field-button");
-	var nextFieldDescriptionNum = num + 1;
-	addFieldButton.onclick = function() { genField( nextFieldDescriptionNum) };
+	var nextFieldDescriptionNum = fdNum + 1;
+	addFieldButton.onclick = function() { genField(nextFieldDescriptionNum); };
 	
 	prevFieldDescription.parentNode.insertBefore(fdDiv, prevFieldDescription.nextSibling);
 }
@@ -116,5 +119,8 @@ window.onload = function() {
 	addFieldButton.onclick = function() { genField(2) };
 
 	var fieldKindSelect = document.getElementById("1-field-kind-select");
-	fieldKindSelect.onchange = function() { genOption(1) };
+	fieldKindSelect.onchange = function() {
+		delOptions(1);
+		genOption(1);
+	};
 }
