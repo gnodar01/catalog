a14c9deed8fc5b2f953ddee9a10114111ca74da7
Nodar Gogoberidze <gnodar01@gmail.com>
2016-04-30 18:23:01 -0400

    Add gdisconnect method

--- catalogizer.py
+++ catalogizer.py
@@ -73,9 +73,9 @@ def gconnect():
         return response
 
     # Check to see if user is already logged in.
-    stored_credentials = login_session.get('credentials')
+    stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps("Current user is already connected."), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -119,6 +119,34 @@ def gconnect():
     flash("Now logged in as %s" % login_session['username'])
     return output    
 
+# DISCONNECT - Revoke a current user's token and reset their login_session.
+def gdisconnect():
+    # Only disconnect a connected user.
+    access_token = login_session['access_token']
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+        
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        del login_session['access_token'] 
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 @app.route('/')
 @app.route('/catalog/')
